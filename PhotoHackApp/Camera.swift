c068f72fba2f25604017563fe5ff0a03a46b26e4
Alex Vihlayew <vihlayew.alex@gmail.com>
2019-09-28 20:20:55 +0300

    Add base functionality

---
+++ PhotoHackApp/Camera.swift
@@ -0,0 +1,63 @@
+//
+//  Camera.swift
+//  PhotoHackApp
+//
+//  Created by Alex on 9/28/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+var device: AVCaptureDevice?
+var captureSession: AVCaptureSession?
+var IMAGE: UIImage?
+
+class Camera: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func setupCamera() {
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .front)
+        device = discoverySession.devices[0]
+        
+        let input: AVCaptureDeviceInput
+        do {
+            input = try AVCaptureDeviceInput(device: device!)
+        } catch {
+            return
+        }
+        
+        let output = AVCaptureVideoDataOutput()
+        output.alwaysDiscardsLateVideoFrames = true
+        
+        let queue = DispatchQueue(label: "cameraQueue")
+        output.setSampleBufferDelegate(self, queue: queue)
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable as! String: kCVPixelFormatType_32BGRA]
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input)
+        captureSession?.addOutput(output)
+        captureSession?.sessionPreset = AVCaptureSession.Preset.photo
+        
+        captureSession?.startRunning()
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        CVPixelBufferLockBaseAddress(imageBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = UnsafeMutableRawPointer(CVPixelBufferGetBaseAddress(imageBuffer!))
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer!)
+        let width = CVPixelBufferGetWidth(imageBuffer!)
+        let height = CVPixelBufferGetHeight(imageBuffer!)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let newContext = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo:
+            CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        
+        let newImage = newContext!.makeImage()
+        IMAGE = UIImage(cgImage: newImage!)
+        
+        CVPixelBufferUnlockBaseAddress(imageBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+    }
+    
+}
--- PhotoHackApp/ViewController.swift
+++ PhotoHackApp/ViewController.swift
@@ -10,11 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputTextFieldMinimumHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var inputContainerViewBottomConstraint: NSLayoutConstraint!
+    
+    let camera = Camera()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    var messages = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        inputTextView.text = "Message"
+        inputTextView.textColor = UIColor.lightGray
+        
+        tableView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        
+        camera.setupCamera()
+        subscribeToKeyboardEvents()
     }
-
-
+    
+    private func subscribeToKeyboardEvents() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillChangeFrameNotification, object: nil, queue: nil) { [weak self] (notification) in
+            if let keyboardFrameRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                self?.inputContainerViewBottomConstraint.constant = (UIScreen.main.bounds.height - keyboardFrameRect.origin.y)
+                self?.view.layoutSubviews()
+            }
+        }
+    }
+    
+    @IBAction func send() {
+        print("TEXT:", inputTextView.text)
+        print("IMAGE:", IMAGE)
+        
+        messages.append(inputTextView.text)
+        inputTextView.text = ""
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+//        cell
+        
+        cell.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+        return cell
+    }
+    
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        sendButton.isEnabled = !textView.text.isEmpty
+        
+        inputTextFieldMinimumHeightConstraint.constant = (textView.contentSize.height > 188) ? 188 : 38
+        textView.isScrollEnabled = (textView.contentSize.height > 188)
+        textView.invalidateIntrinsicContentSize()
+        view.layoutIfNeeded()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Message"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+}
